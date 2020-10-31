9d8428da17d91ae22cf426d1684aaada859340fa
Gagandeep <singh.gagan144@gmail.com>
2020-10-31 13:13:56 +0530

    Heroku Deployment

--- peopleportal/urls.py
+++ peopleportal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from peopleportal import views
 
@@ -28,4 +29,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('conference/', include('conference.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
